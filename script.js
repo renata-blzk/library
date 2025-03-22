edc4cd537317b778cefdd941705e080779362de4
renata-blzk <136249041+renata-blzk@users.noreply.github.com>
2025-03-22 14:56:01 +0000

    change addBookToLibrary func into prototype func

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 const myLibrary = [];
 
 function Book(title, author, pages) {
+    if (!new.target) {
+        throw Error ('use "new" when creating new book');
+    }
     this.title = title;
     this.author = author;
     this.pages = pages;
 }
 
-function addBookToLibrary() {
+Book.prototype.addBookToLibrary = function() {
     let newBook = {
-        title: book.title,
-        author: book.author,
-        pages: book.pages,
+        title: this.title,
+        author: this.author,
+        pages: this.pages,
     }
     
     myLibrary.push(newBook);
     console.log(myLibrary);
 }
 
-const book = new Book('Misery', 'Stephen King', '200');
+const book1 = new Book('Misery', 'Stephen King', '200');
+const book2 = new Book('The Stand', 'Stephen King', '300');
+const book3 = new Book('Lord of the Rigs', 'J.R.R. Tolkien', '500');
 
-addBookToLibrary(book);
+book1.addBookToLibrary();
+book2.addBookToLibrary();
+book3.addBookToLibrary();
+
+
