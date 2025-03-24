8e510e2daeedc4517fb44ea4ec1797a16702441f
renata-blzk <136249041+renata-blzk@users.noreply.github.com>
2025-03-24 20:58:49 +0000

    add submit function to push values to array and close modal with reset

--- script.js
+++ script.js
@@ -1,47 +1,6 @@
 const myLibrary = [];
 
-function Book(title, author, pages, status) {
-    if (!new.target) {
-        throw Error ('use "new" when creating new book');
-    }
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.status = status;
-}
-
-Book.prototype.addBookToLibrary = function() {
-    let newBook = {
-        title: this.title,
-        author: this.author,
-        pages: this.pages,
-        status: this.status,
-    }
-    newBook.id = crypto.randomUUID();
-    
-    myLibrary.push(newBook);
-}
-
-const book1 = new Book('Misery', 'Stephen King', '200', 'read');
-const book2 = new Book('The Stand', 'Stephen King', '300', 'unread');
-const book3 = new Book('Lord of the Rigs', 'J.R.R. Tolkien', '500', 'read');
-
-book1.addBookToLibrary();
-book2.addBookToLibrary();
-book3.addBookToLibrary();
-
-let table = document.querySelector('#tableID');
-
-myLibrary.forEach(book => {
-    let tr = document.createElement('tr');
-    Object.entries(book).forEach(value => {
-        let td = document.createElement('td');
-        td.innerText = value;
-        tr.appendChild(td); 
-    });
-    table.appendChild(tr);
-});
-
+const table = document.querySelector('#tableID');
 const addNewBookBtn = document.querySelector('.addNewBook');
 const formModal = document.querySelector('.formModal');
 const outputBox = document.querySelector('output');
@@ -51,8 +10,33 @@ addNewBookBtn.addEventListener('click', () => {
     formModal.showModal();
 });
 
-submitBtn.addEventListener('click', (event) => {
+document.querySelector('.mainForm').addEventListener('submit', function(event) {
     event.preventDefault();
+
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const status = document.getElementById('status').value;
+
+
+    const book = { title, author, pages, status };
+    
+    book.id = crypto.randomUUID();
+        
+    myLibrary.push(book);
+    
+    myLibrary.forEach(book => {
+        let tr = document.createElement('tr');
+        Object.entries(book).forEach(value => {
+            let td = document.createElement('td');
+            td.innerText = value;
+            tr.appendChild(td); 
+        });
+        table.appendChild(tr);
+    });
+
+    this.reset();
+    formModal.close();
 });
 
 
