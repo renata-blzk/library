160a40acc07810e95dcfb63c3d211dc2adfc34ee
renata-blzk <136249041+renata-blzk@users.noreply.github.com>
2025-03-25 22:09:03 +0000

    add few books to library for display

--- script.js
+++ script.js
@@ -5,6 +5,39 @@ const formModal = document.querySelector('.formModal');
 const outputBox = document.querySelector('output');
 const submitBtn = formModal.querySelector('.submitBtn');
 
+// ADD FEW BOOKS FOR DISPLAY
+function Book(title, author, pages, status) {
+    if (!new.target) {
+        throw Error ('use "new" when creating new book');
+    }
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+}
+
+Book.prototype.addBookToLibrary = function() {
+    let newBook = {
+        title: this.title,
+        author: this.author,
+        pages: this.pages,
+        status: this.status,
+    }
+    newBook.id = crypto.randomUUID();
+
+    myLibrary.push(newBook);
+    document.querySelector('#tableID').innerHTML = myLibrary.map(book => 
+        `<tr><td>Title: ${book.title}</td><td>Author: ${book.author}</td><td>Pages: ${book.pages}</td><td>Status: ${book.status}</td></tr>`).join('');
+}
+
+const book1 = new Book('Misery', 'Stephen King', '200', 'read');
+const book2 = new Book('The Stand', 'Stephen King', '300', 'unread');
+const book3 = new Book('Lord of the Rigs', 'J.R.R. Tolkien', '500', 'read');
+
+book1.addBookToLibrary();
+book2.addBookToLibrary();
+book3.addBookToLibrary();
+
 // OPEN TABLE TO ADD NEW BOOK
 addNewBookBtn.addEventListener('click', () => {
     formModal.showModal();
@@ -40,7 +73,7 @@ document.querySelector('.mainForm').addEventListener('submit', function(event) {
     table.appendChild(tr);*/
 
     ///// this one uses innerHTML - not the best option /////
-    const table = document.querySelector('#tableID').innerHTML = myLibrary.map(book => 
+    document.querySelector('#tableID').innerHTML = myLibrary.map(book => 
         `<tr><td>Title: ${book.title}</td><td>Author: ${book.author}</td><td>Pages: ${book.pages}</td><td>Status: ${book.status}</td></tr>`).join('');
 
     // RESET AND CLOSE THE FORM
